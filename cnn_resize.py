9a163d69d1211833a3ac88bce2dce0e22f7cfad5
htrucci <htrucci@gmail.com>
2018-03-28 00:11:01 +0900

    1주차_완료
    1주차_완료

--- cnn_resize.py
+++ cnn_resize.py
@@ -1,23 +1,40 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import os,sys
 import glob
 
-image_dir = "./한라봉/"
-target_dir = "./한라봉/resize/"
-files = glob.glob(image_dir+"*.*")
-print(len(files))
-count = 1;
-size = (224, 224)
-for file in files:
-    im = Image.open(file)
-    im = im.convert('RGB')
-    print("i: ", count, im.format, im.size, im.mode, file.split("/")[-1])
-    count+=1
-    #imageNew = Image.new("RGB", (224, 224))
-    #imageNew = im.resize((224, 224), resample=3)
-    im.thumbnail(size, Image.ANTIALIAS)
-    #imageNew.paste(image.resize((32, 32), 3))
-    im.save(target_dir+file.split("/")[-1], quality=100)
-#im = Image.open("./한라봉/000001.jpg")
-#im.show()
+fruits = ["귤", "한라봉", "레몬"]
+for fruit_name in fruits:
+    image_dir = "../"+fruit_name+"/"
+    target_resize_dir = "../"+fruit_name+"/resize/"
+    target_rotate_dir = "../"+fruit_name+"/rotate/"
+    if not os.path.isdir(target_resize_dir):
+        os.mkdir(target_resize_dir)
+    if not os.path.isdir(target_rotate_dir):
+        os.mkdir(target_rotate_dir)        
+    files = glob.glob(image_dir+"*.*")
+    print(len(files))
+    count = 1;
+    size = (224, 224)
+    for file in files:
+        im = Image.open(file)
+        im = im.convert('RGB')
+        print("i: ", count, im.format, im.size, im.mode, file.split("/")[-1])
+        #width, height = im.size
+        count+=1
+        #left = width/2 - 112
+        #top = height/2 - 112
+        #right = width/2 + 112
+        #bottom = height/2 + 112
+        #im.crop((left, top, right, bottom))
+        im = ImageOps.fit(im, size, Image.ANTIALIAS, 0, (0.5, 0.5))
+        im.save(target_resize_dir+file.split("/")[-1], quality=100)
+        im.rotate(90).save(target_rotate_dir+file.split("/")[-1], quality=100)
+
+        #imageNew = Image.new("RGB", (224, 224))
+        #imageNew = im.resize((224, 224), resample=3)
+        #im.thumbnail(size, Image.ANTIALIAS)
+        #imageNew.paste(image.resize((32, 32), 3))
+        
+    #im = Image.open("./한라봉/000001.jpg")
+    #im.show()
 
